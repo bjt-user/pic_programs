5c334d7e6a92211232a4644c9c8701064ea1ae02
bjt-user <65905537+bjt-user@users.noreply.github.com>
2020-09-29 20:23:54 +0200

    Create bitshifting.c

---
+++ bitshifting.c
@@ -0,0 +1,39 @@
+/*
+ * File:   bitshifting.c
+ *
+ */
+
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT        // Oscillator Selection bits (LP oscillator: Low-power crystal on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF          // Code Protection bit (Program memory code protection is enabled)
+#pragma config CPD = OFF         // Data Code Protection bit (Data memory code protection is enabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR21V   // Brown-out Reset Selection bit (Brown-out Reset set to 2.1V)
+#pragma config WRT = OFF       // Flash Program Memory Self Write Enable bits (0000h to 0FFFh write protected, 1000h to 1FFFh may be modified by EECON control)
+
+#include <xc.h>
+
+#define _XTAL_FREQ 4000000L
+
+void main(void) {
+    TRISA = 0x00;
+    int led = 0xFF;  //you can assign hex values to an int data type
+    while(1)
+    {
+        PORTA = (char) ~led;
+        led = led << 1;  //you have to assign led to the operation
+        //the bits are shifted by one digit to the left
+        //only zeros come in from the right
+        //the bits don't come back from the right after they are shifted out
+        __delay_ms(1000);
+    }
+    return;
+}
